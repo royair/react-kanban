b15700f313a70ad63ed6ee772c5e5b296dc04436
roy yair <roy.yair@gmail.com>
2019-01-17 19:10:11 +0200

    Updates

---
+++ precache-manifest.67ec5eba1e73a6ac3a9e71465eec57a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec1dba4c7f53d7b74314",
+    "url": "/react-kanban/static/js/runtime~main.ec1dba4c.js"
+  },
+  {
+    "revision": "06cb954037194c13d8fa",
+    "url": "/react-kanban/static/js/main.06cb9540.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/react-kanban/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "06cb954037194c13d8fa",
+    "url": "/react-kanban/static/css/main.a2cbd961.chunk.css"
+  },
+  {
+    "revision": "f1c6ef3516b416170df57c12eb12b4c6",
+    "url": "/react-kanban/index.html"
+  }
+];
